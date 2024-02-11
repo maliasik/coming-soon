38229826a688533fc61dde60d4540937b2836fa3
Muhammet Ali ASIK <maliask06@gmail.com>
2024-02-11 03:28:42 +0300

    coming-soon page for maliasik.net

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import { NgZone } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'coming-soon';
+export class AppComponent implements OnInit, OnDestroy {
+  targetDate = new Date('March 15, 2024 00:00:00').getTime();
+  countdown: string | null = null;
+  private intervalId: any;
+
+  constructor(private zone: NgZone, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.zone.runOutsideAngular(() => {
+      this.intervalId = setInterval(() => {
+        const now = new Date().getTime();
+        const distance = this.targetDate - now;
+        this.zone.run(() => {
+          if (distance < 0) {
+            this.countdown = "Zaman doldu!";
+            clearInterval(this.intervalId);
+          } else {
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            this.countdown = `${days} gün ${hours} saat ${minutes} dakika ${seconds} saniye`;
+          }
+        });
+      }, 1000);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  private updateCountdown(): void {
+    const now = new Date().getTime();
+    const distance = this.targetDate - now;
+    if (distance < 0) {
+      this.countdown = "Zaman doldu!";
+      if (this.intervalId) {
+        clearInterval(this.intervalId);
+      }
+      return;
+    }
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    this.countdown = `${days} gün ${hours} saat ${minutes} dakika ${seconds} saniye`;
+    this.cdr.markForCheck(); 
+  }
 }
